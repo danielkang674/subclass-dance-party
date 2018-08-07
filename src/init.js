95960388a22a4c3ff9b15f0cdce3ec7081eb59f0
Hack Reactor Student <student@hackreactor.com>
2018-08-07 18:23:10 -0500

    Add hover color changing and music

--- src/init.js
+++ src/init.js
@@ -1,20 +1,10 @@
 $(document).ready(function() {
   window.dancers = [];
-
+  // if (window.dancers.length > 0) {
+  //   $('span.user-position.user-triangle').remove();
+  //   $('span.space-invader.space-invader-ufo').remove();
+  // }
   $('.addDancerButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
-
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
     // get the maker function for the kind of dancer we're supposed to make
@@ -30,8 +20,24 @@ $(document).ready(function() {
     $('body').append(dancer.$node);
   });
 
-  // make alien
+});
+
+
+  // BASIC MODE
   $('.spaceInvaderButton').on('click', function(event) {
+    // Link to Mercury audio
+    $('audio').each(function() {
+      this.pause();
+      this.currentTime = 0;
+    });
+    $('audio#mercury')[0].play();
+
+    // check if the window dancer is empty, if not, remove all elements
+    if (window.dancers.length > 0) {
+      $('span.user-position.user-triangle').remove();
+      $('span.space-invader.space-invader-ufo').remove();
+      window.dancers.pop();
+    }
     // calls upon the function within the html element
     var alienMakerFunctionName = $(this).data('alien-maker-function-name');
     // calls upon the window object with the stringed key of our Alien and playerShip functions
@@ -40,12 +46,12 @@ $(document).ready(function() {
     var playerShipMakerFunction = window[gameFunctionArray[1]];
 
     //
-    var alien = new alienMakerFunction(250, 210, 1000);
     let firstAlienTop = 140;
     let firstAlienLeft = 120;
 
     var player = new playerShipMakerFunction(690, 440, 1000);
     $('#gameScreen').append(player.$node);
+    window.dancers.push(player);
 
     for(let i = 0; i < 4; i++) {
       let firstAlien = new alienMakerFunction(firstAlienTop, firstAlienLeft, 50);
@@ -58,6 +64,73 @@ $(document).ready(function() {
     window.firstAlienRow = 200;
     window.stepRight = true;
 
+    // Hover color change requirement
+    $( "span.user-position.user-triangle" ).mouseover(function() {
+      $(this).css("border-bottom-color", "#587079");
+    $( "span.user-position.user-triangle" ).mouseout(function() {
+        $(this).css( "border-bottom-color", "#fff" );
+    });
   });
 });
-  // make character at the bottom of the screen upon the same event as alien
+  document.addEventListener('keydown', function(event){
+    var player = window.dancers[0];
+    const key = event.key;
+    if(key === 'ArrowLeft' && player.left >= 200){
+      player.left -= 90;
+      player.setPosition();
+    } else if (key === 'ArrowRight' && player.left <= 800){
+      player.left += 90;
+      player.setPosition();
+    }
+  });
+
+
+$('.massacreButton').on('click', function(event) {
+  // stops all music
+  $('audio').each(function() {
+    this.pause();
+    this.currentTime = 0;
+  });
+  // plays uranus song
+  $('audio#uranus')[0].play();
+
+
+  if (window.dancers.length > 0) {
+    $('span.user-position.user-triangle').remove();
+    $('span.space-invader.space-invader-ufo').remove();
+    window.dancers.pop();
+  }
+  // calls upon the function within the html element
+  var alienMakerFunctionName = $(this).data('massacre-maker-function-name');
+  // calls upon the window object with the stringed key of our Alien and playerShip functions
+  var gameFunctionArray = alienMakerFunctionName.split(" ");
+  var alienMakerFunction = window[gameFunctionArray[0]];
+  var playerShipMakerFunction = window[gameFunctionArray[1]];
+
+  var player = new playerShipMakerFunction(690, 440, 1000);
+  $('#gameScreen').append(player.$node);
+  window.dancers.push(player);
+
+  let firstAlienTop = 140;
+
+  for(let i = 0; i < 7; i++) {
+    let firstAlienLeft = 120;
+    for (let j = 0; j < 7; j++) {
+      let firstAlien = new alienMakerFunction(firstAlienTop, firstAlienLeft, 600000000);
+      $('#gameScreen').append(firstAlien.$node);
+      firstAlienLeft += 100;
+    }
+    firstAlienTop += 50;
+  }
+  window.firstAlienRow = 200;
+  window.stepRight = true;
+
+  // Hover color change requirement
+  $( "span.user-position.user-triangle" ).mouseover(function() {
+    $(this).css("border-bottom-color", "#957838");
+  $( "span.user-position.user-triangle" ).mouseout(function() {
+      $(this).css( "border-bottom-color", "#fff" );
+  });
+
+  });
+});
